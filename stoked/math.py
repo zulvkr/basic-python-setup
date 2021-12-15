a1d3fc4cf8bae4ef242d13c433f99424c49b058e
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2021-12-15 22:25:16 +0700

    add MyPy

--- stoked/math.py
+++ stoked/math.py
@@ -1,2 +1,8 @@
-def multiply_two_numbers(a, b):
+from numbers import Real
+from typing import Union
+
+
+def multiply_two_numbers(
+    a: Union[int, Real], b: Union[int, Real]
+) -> Union[int, Real]:
     return a * b
