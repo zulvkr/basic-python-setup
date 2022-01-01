c5773120b77bbd34928681305052c2cd619ca2b0
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2022-01-01 19:25:38 +0700

    unbreak

--- fast/main.py
+++ fast/main.py
@@ -140,5 +140,4 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 async def read_own_items(
     current_user: User = Depends(get_current_active_user),
 ):
-    return [{"item_id
-    ": "Foo", "owner": current_user.username}]
+    return [{"item_id": "Foo", "owner": current_user.username}]
